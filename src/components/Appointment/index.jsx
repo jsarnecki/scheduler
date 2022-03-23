c5aec95d05326e8606bd6a2f8b366a140426227a
Joshyjosh <joshsarnecki@gmail.com>
2022-03-23 14:49:16 -0700

    Began adding components to index.jsx file.  By adding Appointment Booked into storybook, we are passed an interview object as a prop to set up the Appointment comp.  Adding a header to every Appointment, checking if there is an interview prop available, and using Show comp, or if not using Empty prop.  Passing the interview obj details into the Show comp.  Tested with storybook

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import "./styles.scss";
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
 export default function Appointment(props) {
 
-  const { time } = props;
+  const { time, interview } = props;
 
   return (
     <>
+    <Header time={time} />
       {time ? 
       <article time={time} className="appointment">Appointment at {time}</article> 
         :
       <article time={time} className="appointment">No Appointments</article> 
       }
+        {interview ? 
+        <Show student={interview.student} interviewer={interview.interviewer.name}/>
+          :
+        <Empty />
+        }
     </>
   );
 }
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import DayList from "components/DayList.jsx";
 import DayListItem from "components/DayListItem.js";
@@ -181,4 +181,20 @@ storiesOf("Appointment", module)
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ))
