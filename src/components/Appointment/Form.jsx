cd2e9ed88893a67e4590b4790470fcf6059d5bd9
Joshyjosh <joshsarnecki@gmail.com>
2022-03-23 10:01:00 -0700

    Started on Form component, and adding the stories, need to make the input into a controlled component

---
+++ src/components/Appointment/Form.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "./styles.scss";
+import InterviewerList from 'components/InterviewerList';
+import Button from 'components/Button';
+
+export default function Form(props) {
+  const { student, interviewer, setStudent, setInterviewer, interviewers, onSave, onCancel } = props;
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+              This must be a controlled component
+              your code goes here
+            */
+          />
+        </form>
+        <InterviewerList 
+          interviewers={interviewers}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={onCancel}>Cancel</Button>
+          <Button confirm onClick={onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -11,6 +11,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -164,3 +165,19 @@ storiesOf("Appointment", module)
   ))
   .add("Status", () => <Status message="Deleting" />)
   .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
+  .add("Edit Form", () => (
+    <Form
+      student="Joshyboy"
+      interviewer={2}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Create Form", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
