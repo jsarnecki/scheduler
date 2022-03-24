f53a4b9b4f345a2909ae8d0b6b17bec7b282f4df
Joshyjosh <joshsarnecki@gmail.com>
2022-03-24 16:21:50 -0700

    Brought in the appointments data with axios, appending it to the original get days Promise, using Promise.all.  Setting the state for both days and appointments.  Also brought in interviewers, but haven't fully implemented it yet

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import axios from 'axios';
+import { getAppointmentsForDay } from "helpers/selectors";
 
 import "components/Application.scss";
 
@@ -49,21 +50,31 @@ export default function Application(props) {
 
   const [state, setState] = useState({
     day: "Monday",
-    days: []
-    //appointments: {}
+    days: [],
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({ ...prev, days}));
+  //const setDays = days => setState(prev => ({ ...prev, days}));
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days").then((res) => {
-      setDays(res.data);
-      
-    })
-  });
 
-  const mappedAppointments = Object.values(appointments).map((appt) => {
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ]).then((all) => {
+      console.log(all);
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
+  
+    });
+  }, []);
+
+  const mappedAppointments = dailyAppointments.map((appt) => {
+
     return <Appointment 
               key={appt.id}
               {...appt}
