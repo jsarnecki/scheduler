6de2bdebb8d75b9f60ee4f7238a926cd236bf8f3
Joshyjosh <joshsarnecki@gmail.com>
2022-03-26 14:38:08 -0700

    Refactored getAppointmentsForDay selector which had lots of nested loops.  Now has better readability

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,27 +1,50 @@
-
+// export function getAppointmentsForDay(state, day) {
+//   const filteredApptsForDay = [];
+//   for (const d of state.days) {
+//     //Loop thru days objs
+//     if (d.name === day) {
+//       //Find day name matched with given day arg
+//       for (let id of d.appointments) {
+//         //Loop thru the appointment ids of matched day
+//         for (let obj in state.appointments) {
+//           //Loop thru state.appointments obj to match against day ids
+//           if (id == obj) {
+//             //If the id of the days appointment matches the state.appoiments id, push into arr
+//             filteredApptsForDay.push(state.appointments[obj]);
+//           }
+//         }
+//       }
+//     }
+//   }
+//   return filteredApptsForDay;
+// }
 
 export function getAppointmentsForDay(state, day) {
-  const filteredApptsForDay = [];
-
+  let appointmentIdArr = [];
   for (const d of state.days) {
-    //Loop thru days objs
     if (d.name === day) {
-      //Find day name matched with given day arg
-      for (let id of d.appointments) {
-        //Loop thru the appointment ids of matched day
-        for (let obj in state.appointments) {
-          //Loop thru state.appointments obj to match against day ids
-          if (id == obj) {
-            //If the id of the days appointment matches the state.appoiments id, push into arr
-            filteredApptsForDay.push(state.appointments[obj]);
-          }
-        }
-      }
+      // If day arg matches day elm, spread all appointmentIds
+      appointmentIdArr = [...d.appointments];  
     }
   }
 
-  return filteredApptsForDay;
+  if (!appointmentIdArr.length) {
+    //If no days match, return empty
+    return [];
+  }
+
+  const appointmentsObjArr = Object.values(state.appointments);
+  //Take the appointments obj of objs into arr
+  const appointmentsReturn = appointmentsObjArr.filter(apt => {
+    //Filter out the ids that match with given day apptIds
+    if (appointmentIdArr.includes(apt.id)) {
+      return apt;
+    }
+  });
+
+  return appointmentsReturn;
 }
+
 
 export function getInterview(state, interview) {
   if (!interview) {
